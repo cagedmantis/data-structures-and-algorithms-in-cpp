1c38b18446618671052837ab70c74cb49d98cc80
cagedmantis <carlos.amedee@gmail.com>
2012-05-09 14:35:23 -0400

    Added insertion sort.

---
+++ algorithms/sorting/insertionsort.cpp
@@ -0,0 +1,23 @@
+// Algorithm: Insertion Sort
+// Complexity: O(n^2)
+
+#include <vector>
+#include <iostream>
+
+template <typename T>
+void insertionSort( std::vector<T> &vec)
+{
+  for (size_t index1 = 0; index1 < vec.size(); ++index1)
+    {
+      size_t minIndex = index1;
+      for (size_t index2 = index1; index2 < vec.size(); ++index2)
+        {
+          if ( vec[minIndex] > vec[index2] )
+            {
+              minIndex = index2;
+            }
+        }
+      std::swap( vec[index1], vec[minIndex] );
+    }
+}
+
